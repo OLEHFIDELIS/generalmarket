99f1c03f659792d0305cfb093d4ebe6f8115b697
Oleh Fidelis <olehfidelis3609@icloud.com>
2025-10-30 07:30:35 +0100

    worked on the category section

--- frontend/src/Pages/Shop.jsx
+++ frontend/src/Pages/Shop.jsx
@@ -4,6 +4,7 @@ import Popular from "../Components/Popular/Popular"
 import Offers from "../Components/Offers/Offers"
 import NewCollections from "../Components/NewCollections/NewCollections"
 import NewsLetter from "../Components/NewsLetter/NewsLetter"
+import Category from "../Components/Categories/Category"
 import Footer from "../Components/Footer/Footer"
 import Search from "../Components/Search/Search"
 import Welcome from "../Components/Welcome/Welcome"
@@ -14,7 +15,7 @@ const Shop = ()=> {
            <Hero/>
            <Welcome/>
            <Popular/>
-           <Offers/>
+           <Category/>
            <NewCollections/>
            <NewsLetter/>
         </div>
