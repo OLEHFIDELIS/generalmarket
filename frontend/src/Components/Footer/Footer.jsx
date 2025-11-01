3882df5811f9d4a5f52b1c3c9bfc71e8ab7753f3
Oleh Fidelis <olehfidelis3609@icloud.com>
2025-11-01 07:55:05 +0100

    worked on the footer

--- frontend/src/Components/Footer/Footer.jsx
+++ frontend/src/Components/Footer/Footer.jsx
@@ -1,45 +1,75 @@
 import React from "react";
-import './Footer.css';
-import footer_logo from '../Assets/logo_big.png';
-import instagram_icon from '../Assets/instagram_icon.png'
-import pintester_icon from '../Assets/pintester_icon.png';
-import whatsapp_icon from '../Assets/whatsapp_icon.png';
+import "./Footer.css";
+import { FaWhatsapp, FaFacebookF, FaPinterestP, FaInstagram, FaTwitter } from "react-icons/fa";
 
-
-
-const Footer = ()=> {
-    return(
-        <div className="footer">
-            <div className="footer-logo">
-                <img src={footer_logo} alt="" />
-                <p>SHOPPER</p>
-
-            </div>
-            <ul className=" footer-links">
-                <li>Company</li>
-                <li>Products</li>
-                <li>Offices</li>
-                <li>About</li>
-                <li>Contact</li>
-            </ul>
-            <div className="footer-social-icon">
-                <div className="footer-icon-container">
-                    <img src={instagram_icon} alt="" />
-                </div>
-                <div className="footer-icon-container">
-                    <img src={pintester_icon} alt="" />
-                </div>
-                <div className="footer-icon-container">
-                    <img src={whatsapp_icon} alt="" />
-                </div>
-            </div>
-            <div className="footer-copyright">
-                <hr />
-                <p>Copyright &copy; 2025 - All Right Reserved</p>
-                
-            </div>
+const Footer = () => {
+  return (
+    <footer className="faji-footer">
+      <div className="faji-top">
+        <div className="faji-company">
+          <div className="faji-logo">G-Market<span>.ng</span></div>
+          <h3>General-Market</h3>
+          <p className="faji-contact">
+            Phone: <a href="tel:07025187910">07025187910</a><br />
+            Email: <a href="mailto:info@faji.ng">info@faji.ng</a><br />
+            NO 8 John Adollo Ikeja Lagos State
+          </p>
+          <p className="faji-desc">
+            G-markert.ng is Nigeria’s trusted online classifieds platform, connecting buyers and sellers across the country.
+            From jobs and services to vehicles, electronics, and real estate, G-market.ng makes it easy to find great deals 
+            and grow your business. <br />Safe. Simple. Local.
+          </p>
+          <button className="dark-mode-btn">☾ Switch to dark mode</button>
         </div>
-    )
+
+        <div className="faji-social">
+          <h4>Social media</h4>
+          <ul>
+            <li><FaWhatsapp className="icon" /> Whatsapp</li>
+            <li><FaFacebookF className="icon" /> Facebook</li>
+            <li><FaPinterestP className="icon" /> Pinterest</li>
+            <li><FaInstagram className="icon" /> Instagram</li>
+            <li><FaTwitter className="icon" /> Twitter</li>
+          </ul>
+        </div>
+
+        <div className="faji-info">
+          <h4>Information</h4>
+          <ul>
+            <li>About Us</li>
+            <li>How It Works</li>
+            <li>Safety Tips</li>
+            <li>FAQ</li>
+            <li>Terms of Service</li>
+            <li>Privacy Policy</li>
+            <li>Refund Policy</li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="faji-middle">
+        <p>
+          Welcome to G-Marke.ng – your trusted platform for buying, selling, exchanging and trading across Nigeria. 
+          Post free ads and reach thousands of buyers instantly.
+        </p>
+      </div>
+
+      <div className="faji-bottom">
+        <div className="faji-line"></div>
+        <div className="faji-bottom-links">
+          <div className="left">
+            <a href="#">Contact us</a> &nbsp;|&nbsp;
+            <a href="#">General Market</a> &nbsp;|&nbsp;
+            <a href="">Android App</a>
+          </div>
+          <div className="right">
+            Company Registration: RC 8626841<br />
+            Copyright © 2025 GeneralMarket Digital Services Limited. All rights reserved.
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
