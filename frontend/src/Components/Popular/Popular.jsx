d50fc02842d9476f053d244ffadccf3e2e15c0be
Oleh Fidelis <olehfidelis3609@icloud.com>
2025-11-21 00:01:24 +0100

    made some changes on uploads

--- frontend/src/Components/Popular/Popular.jsx
+++ frontend/src/Components/Popular/Popular.jsx
@@ -6,7 +6,7 @@ const Popular = () => {
   const [popularProducts, setPopularProducts] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:4000/allproduct")
+    fetch("https://generalmarket-bruz.onrender.com/allproduct")
       .then((response) => response.json())
       .then((data) => setPopularProducts(data));
   }, []);
