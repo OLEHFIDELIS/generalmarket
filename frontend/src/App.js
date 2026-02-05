b33aa7404c0a33dcaae7ef92ac238a8bd209896b
Oleh Fidelis <olehfidelis3609@icloud.com>
2026-02-05 19:10:21 +0100

    worked on routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,44 +1,44 @@
 import './App.css';
-import {BrowserRouter,Routes,Route} from "react-router-dom"
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Shop from './Pages/Shop';
 import ShopCategory from './Pages/ShopCategory';
 import LoginSignup from './Pages/LoginSignup';
 import Product from './Pages/Product';
-import Cart from './Pages/Cart'
+import Cart from './Pages/Cart';
 import Footer from './Components/Footer/Footer';
-import NewNav from './Components/NewNav/NewNav'
+import NewNav from './Components/NewNav/NewNav';
 import { categories } from "./data/categories";
-import Navbar from './Components/Navbar/Navbar';
-
 
 function App() {
   return (
     <div>
-      <BrowserRouter>
-      <NewNav/> 
-      {/* <Navbar />   */}
+      <HashRouter>
+        <NewNav />
 
-      <Routes>
-        <Route path="/" element={<Shop />} />
-        {/* AUTO-GENERATE CATEGORY ROUTES */}
-        {categories.map((cat, index) => (
-          <Route
-            key={index}
-            path={`/category/${cat.toLowerCase().replace(/ & /g, "-").replace(/ /g, "-")}`}
-            element={<ShopCategory category={cat} />}
-          />
-        ))}
+        <Routes>
+          <Route path="/" element={<Shop />} />
 
-        {/* Product routes */}
-        <Route path='/product'>
-          <Route index element={<Product />} />
-          <Route path=":productId" element={<Product />} />
-        </Route>
-        <Route path='/cart' element={<Cart />} />
-        <Route path='/login' element={<LoginSignup />} />
-      </Routes>
-      <Footer/>
-    </BrowserRouter>
+          {/* Category routes */}
+          {categories.map((cat, index) => (
+            <Route
+              key={index}
+              path={`/category/${cat
+                .toLowerCase()
+                .replace(/ & /g, "-")
+                .replace(/ /g, "-")}`}
+              element={<ShopCategory category={cat} />}
+            />
+          ))}
+
+          {/* Product routes */}
+          <Route path="/product/:productId" element={<Product />} />
+
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<LoginSignup />} />
+        </Routes>
+
+        <Footer />
+      </HashRouter>
     </div>
   );
 }
